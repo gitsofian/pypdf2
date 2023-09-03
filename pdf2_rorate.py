a3985835760ffd952a4baf090991922a147b7dfe
sofiane <sofiane@meriane.dev>
2023-09-03 21:35:04 +0200

    rorate func PyPDF2 3.0.1 version

---
+++ pdf2_rorate.py
@@ -0,0 +1,17 @@
+import PyPDF2
+
+# exchange the 'original.pdf' with a name of your file
+pdfIn = open('original.pdf', 'rb')
+pdfReader = PyPDF2.PdfReader(pdfIn)
+pdfWriter = PyPDF2.PdfWriter()
+num_of_pages = len(pdfReader.pages)
+
+for pageNum in range(num_of_pages):
+    page = pdfReader.pages[pageNum]
+    page.rotate(180)
+    pdfWriter.add_page(page)
+
+pdfOut = open('rotated.pdf', 'wb')
+pdfWriter.write(pdfOut)
+pdfOut.close()
+pdfIn.close()
